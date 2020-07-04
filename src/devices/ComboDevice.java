5c7eda3dbcba5a07f0d2888d374784dfdaed84d6
Roberto Jesus <roberto.jesus.amaral@gmail.com>
2020-07-04 12:20:35 -0300

    Problema diamante - Usando interface para poder implementar 2 interfaces na mesma classe

--- src/devices/ComboDevice.java
+++ src/devices/ComboDevice.java
@@ -1,8 +1,25 @@
 package devices;
-						//não permite extender 2 classes
-public class ComboDevice  extends Scanner{
+						//não permite extender 2 classes mas pode implementar 2 interfaces
+public class ComboDevice  extends Device implements Scanner, Printer{
 	
 	public ComboDevice(String serialNumber) {
 		super(serialNumber);
+	}
+
+	@Override
+	public void print(String doc) {
+		System.out.println("Combo printing: " + doc);
+		
+	}
+
+	@Override
+	public String scan() {
+		return "Combo scan result";
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Combo processing: " + doc);
+		
 	}
 }
